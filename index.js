4d333c05be3a419b967e92a29338a20c8d30749b
jalpa Shah <jalpa-shah@lambdastudents.com>
2020-12-14 18:19:13 -0500

    all the tests are done

--- index.js
+++ index.js
@@ -29,10 +29,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   Study the code for counter1 and counter2, then answer the questions below.
   
-  1. What is the difference between counter1 and counter2?
-  
+  1. What is the difference between counter1 and counter2?  
+  Ans: counter1 is an object. counter2 is a fuction.
+
   2. Which of the two uses a closure? How can you tell?
-  
+  Ans: counter1 is a closure as it can access the counterMaker() variable.
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -66,8 +68,10 @@ NOTE: This will be a callback function for the tasks below
 
 function inning(/*Code Here*/){
     /*Code Here*/
-}
 
+    return Math.floor(Math.random() * 3);
+}
+console.log(inning());
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -83,18 +87,31 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inning, number/*code Here*/){
   /*Code Here*/
+  let homeTeam = 0;
+  let awayTeam = 0;
+  //const currentTeamScore = inning(number);
+  for(let i=0; i<number; i++){
+    homeTeam = inning() + homeTeam; 
+    awayTeam = inning() + awayTeam;
+  }
+  return {'Home': homeTeam, 'Away':awayTeam}
 }
+console.log(finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
+function getInningScore(inning/*Your Code Here */) {
   /*Your Code Here */
+  let homeTeam = 0;
+  let awayTeam = 0;
+  return {'Home':homeTeam, 'Away':awayTeam}
 }
+console.log(getInningScore(inning()));
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
